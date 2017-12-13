932f88c8b4993476bf26074ce92d85c62c53497e
Eric Tobin <eatobin@gmail.com>
2017-12-13 16:53:44 -0600

    progress

--- src/Main.hs
+++ src/Main.hs
@@ -2,31 +2,84 @@
 
 module Main where
 
-import           System.Random
--- import Control.Monad
+import           Control.Monad.Loops (whileM_)
+import           System.Random       (randomRIO)
 
-promptLine :: String -> IO String
-promptLine prompt = do
-  putStr prompt
-  getLine
+-- main :: IO ()
+-- main = do
+--   putStrLn "Guess the number!"
+--   secretNumber <- randomRIO (1, 100 :: Int)
+--   putStrLn("The secret number is: " ++ show secretNumber)
+--   putStrLn "Please input your guess."
+--   guess <- getLine
+--   putStrLn ("You guessed: " ++ guess)
+--   case compare (read guess) secretNumber of
+--     LT ->  putStrLn "Too small!"
+--     GT -> putStrLn "Too big!"
+--     EQ -> putStrLn "You win!"
+
+
 
 main :: IO ()
 main = do
   putStrLn "Guess the number!"
   secretNumber <- randomRIO (1, 100 :: Int)
   putStrLn("The secret number is: " ++ show secretNumber)
-  guess <- promptLine "Please input your guess.\n"
-  putStrLn ("You guessed: " ++ guess)
-  case compare (read guess) secretNumber of
-    LT ->  putStrLn "Too small!"
-    GT -> putStrLn "Too big!"
-    EQ -> putStrLn "You win!"
+  putStrLn "Please input your guess."
+  whileM_
+    (do
+      guess <- getLine
+      return (guess /= show secretNumber))
+    (do
+      putStrLn ("You guessed: " ++ guess)
+      putStrLn ("One more time... You guessed: " ++ guess))
+  putStrLn "Bye"
 
 
 
--- main = do
---   guess <- promptLine "Please input your guess.\n"
---   unless (guess == "q") $ do
---     putStrLn "1"
---     putStrLn "2"
---     main
+-- | No explicit recursion
+logIn2 :: IO ()
+logIn2 = do
+  putStrLn "% Enter password:"
+  whileM_
+    (do
+      guess <- getLine
+      return (guess /= "secret"))
+    (do
+      putStrLn "% Wrong password!"
+      putStrLn "% Try again:")
+  putStrLn "$ Congratulations!"
+
+-- | With $ syntax.
+logIn3 :: IO ()
+logIn3 = do
+  putStrLn "% Enter password:"
+  whileM_
+    (do
+      guess <- getLine
+      return (guess /= "secret")) $
+    do
+    putStrLn "% Wrong password!"
+    putStrLn "% Try again:"
+  putStrLn "$ Congratulations!"
+
+-- | With operator sectioning and <$>.
+logIn5 :: IO ()
+logIn5 = do
+  putStrLn "% Enter password:"
+  whileM_ ((/= "secret") <$> getLine) $ do
+    putStrLn "% Wrong password!"
+    putStrLn "% Try again:"
+  putStrLn "$ Congratulations!"
+
+guess :: IO ()
+guess = do
+  putStrLn "Please input your guess."
+  whileM_
+    (do
+      guess <- getLine
+      return (guess /= "secret"))
+    (do
+      putStrLn ("You guessed: " ++ guess)
+      putStrLn ("And " ++ guess ++ " is wrong."))
+  putStrLn "Right - Bye..."
