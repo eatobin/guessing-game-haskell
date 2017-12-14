ce1824d2a928ceb9ce5666857cf18e3e9311840a
Eric Tobin <eatobin@gmail.com>
2017-12-14 17:11:09 -0600

    broken

--- src/Main.hs
+++ src/Main.hs
@@ -4,23 +4,28 @@ module Main where
 
 import           Control.Monad.Loops (whileJust_)
 import           System.Random       (randomRIO)
+import           Text.Read (readMaybe)
 
 main :: IO ()
 main = do
   putStrLn "Guess the number!"
   secretNumber <- randomRIO (1, 100 :: Int)
-  putStrLn("The secret number is: " ++ show secretNumber)
+  putStrLn ("The secret number is: " ++ show secretNumber)
   whileJust_
-    (do
+    ( do
       putStrLn "Please input your guess."
       guess <- getLine
-      return $ if read guess /= secretNumber
-               then Just guess else Nothing)
-    (\guess -> do
-      putStrLn $ "You guessed: " ++ guess
-      if read guess < secretNumber
-        then
-          putStrLn "Too small!"
-        else
-          putStrLn "Too big!")
+      return $ if (readMaybe guess :: Maybe Int) /= Just secretNumber then (readMaybe guess :: Maybe Int) else Nothing
+    )
+    ( \guess -> do
+      putStrLn $ "You guessed: " ++ show guess
+      if guess < secretNumber
+        then putStrLn "Too small!"
+        else putStrLn "Too big!"
+    )
   putStrLn "You win!"
+
+
+-- if (readMaybe "77" :: Maybe Int) == Just 77 then Just 77 else Nothing
+-- if (readMaybe "dd" :: Maybe Int) == Just 77 then Just 77 else Nothing
+-- if (readMaybe "dd" :: Maybe Int) /= Just 77 then Just 77 else Nothing
