7b11e4ae7a8cb2dd7861980678026e8bbb105ade
Eric Tobin <eatobin@gmail.com>
2018-01-28 21:36:33 -0800

    reformat

--- src/Main.hs
+++ src/Main.hs
@@ -12,18 +12,18 @@ main = do
   secretNumber <- randomRIO (1, 100 :: Int)
   putStrLn ("The secret number is: " ++ show secretNumber)
   go secretNumber
- where
-  go sn = do
-    putStrLn "Please input your guess."
-    guess <- getLine
-    let validNum = readMaybe guess :: Maybe Int
-    if isJust validNum
-      then case compare (fromJust validNum) sn of
-        LT -> do
-          putStrLn "Too small!"
-          go sn
-        GT -> do
-          putStrLn "Too big!"
-          go sn
-        EQ -> putStrLn "You win!"
-      else go sn
+    where
+      go sn = do
+        putStrLn "Please input your guess."
+        guess <- getLine
+        let validNum = readMaybe guess :: Maybe Int
+        if isJust validNum
+          then case compare (fromJust validNum) sn of
+            LT -> do
+              putStrLn "Too small!"
+              go sn
+            GT -> do
+              putStrLn "Too big!"
+              go sn
+            EQ -> putStrLn "You win!"
+          else go sn
