342440ef1545c778a539da9210c1897d213bf2a2
Eric Tobin <eatobin@gmail.com>
2019-11-30 14:50:36 -0600

    compact

--- src/Main.hs
+++ src/Main.hs
@@ -4,30 +4,30 @@ module Main where
 
 import           Control.Monad
 import           Data.Maybe
-import           System.Random                  ( randomRIO )
-import           Text.Read                      ( readMaybe )
+import           System.Random (randomRIO)
+import           Text.Read     (readMaybe)
 
 main :: IO ()
 main = do
-        putStrLn "Guess the number!"
-        secretNumber <- randomRIO (1, 100 :: Int)
-        putStrLn ("The secret number is: " ++ show secretNumber)
-        go secretNumber
-    where
-        go sn = do
-                putStrLn "Please input your guess."
-                guess <- getLine
-                let validNum = readMaybe guess :: Maybe Int
-                when
-                        (isJust validNum)
-                        (putStrLn ("You guessed: " ++ show (fromJust validNum)))
-                if isJust validNum
-                        then case compare (fromJust validNum) sn of
-                                LT -> do
-                                        putStrLn "Too small!"
-                                        go sn
-                                GT -> do
-                                        putStrLn "Too big!"
-                                        go sn
-                                EQ -> putStrLn "You win!"
-                        else go sn
+  putStrLn "Guess the number!"
+  secretNumber <- randomRIO (1, 100 :: Int)
+  putStrLn ("The secret number is: " ++ show secretNumber)
+  go secretNumber
+  where
+    go sn = do
+      putStrLn "Please input your guess."
+      guess <- getLine
+      let validNum = readMaybe guess :: Maybe Int
+      when
+        (isJust validNum)
+        (putStrLn ("You guessed: " ++ show (fromJust validNum)))
+      if isJust validNum
+        then case compare (fromJust validNum) sn of
+               LT -> do
+                 putStrLn "Too small!"
+                 go sn
+               GT -> do
+                 putStrLn "Too big!"
+                 go sn
+               EQ -> putStrLn "You win!"
+        else go sn
