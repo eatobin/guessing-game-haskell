beed3f1404fa45cd06714c023dfa51be92a06868
Eric Tobin <eatobin@gmail.com>
2019-12-28 10:37:48 -0600

    changes

--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,7 @@ main :: IO ()
 main = do
   putStrLn "Guess the number!"
   secretNumber <- randomRIO (1, 100 :: Int)
-  putStrLn ("The secret number is: " ++ show secretNumber)
+  putStrLn ("Sweetie, the secret number is: " ++ show secretNumber)
   go secretNumber
   where
     go sn = do
